53c10ba82e8136be1c1aea969fca18214879a898
Mert Emir <48881002+mertemr@users.noreply.github.com>
2022-10-28 17:09:22 +0300

    gui updates

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import json
+from select import select
 import sys
 from typing import List
 
@@ -33,7 +34,8 @@ class Window(QtWidgets.QMainWindow):
     def initialize(self):
         self.expires_at = 0
         self.spotify = Spotify()
-        self.playlists_list = []
+        self.playlists_dict = {}
+        self.last_path = str(CWD)
         
         self.statusBar: QtWidgets.QStatusBar
         self.button_getPlaylists: QtWidgets.QPushButton
@@ -48,33 +50,70 @@ class Window(QtWidgets.QMainWindow):
         self.button_export.clicked.connect(self.exportPlaylist)
         self.button_getPlaylists.clicked.connect(self.getPlaylists)
         
+        self.selected_tree = None
         self.playlists_tree: QtWidgets.QTreeView
-    
+        # self.playlists_tree.clicked.connect(self.changeSelected)
+        # self.playlists_tree.connect(self.changeSelected)
+        
+       
+    def changeSelected(self):
+        x = self.playlists_tree.currentIndex()
+        if self.playlists_tree.rootIndex():
+            self.selected_tree = x.parent()
+        else:
+            self.selected_tree = x
+        
+        print(self.selected_tree.data())
+        # for i in self.selected_tree:
+
     def importPlaylist(self):
         file = QtWidgets.QFileDialog.getOpenFileName(
             self,
             "Open File",
-            str(CWD),
+            self.last_path,
             "Playlist Json File (*.json)"
         )[0]
         
-        if file:
-            try:
-                with open(file, 'r', encoding="UTF-8") as f:
-                    playlists: dict = json.load(f)
-            except json.JSONDecodeError:
-                return msgbox("File is not valid.", 16)
-                
-            try:
-                playlists.values()[0]['tracks']
-            except KeyError:
-                return msgbox("Backup file is not valid.", 16)
+        if not file:
+            return
         
-            return self.setTreeView(playlists)
-        
-    def exportPlaylist(self):
-        pass
+        self.last_path = str(Path(file).absolute())
+        try:
+            with open(file, 'r', encoding="UTF-8") as f:
+                playlists: dict = json.load(f)
+        except json.JSONDecodeError:
+            return msgbox("File is not valid.", 16)
+            
+        try:
+            list(playlists.values())[0]['tracks']
+        except KeyError:
+            return msgbox("Backup file is not valid.", 16)
+
+        self.button_export.setEnabled(True)
+        return self.setTreeView(playlists)
     
+    def exportPlaylist(self):
+        if not self.playlists_dict:
+            return msgbox("No playlist found.", 32)
+        
+        file = QtWidgets.QFileDialog.getSaveFileName(
+            self,
+            "Save playlist",
+            self.last_path,
+            "Playlist Json File (*.json)"
+        )[0]
+        
+        if not file:
+            return
+
+        self.last_path = str(Path(file).absolute())
+        
+        try:
+            with open(file, 'w', encoding="UTF-8") as f:
+                json.dump(self.playlists_dict, f, indent=4)
+        except OSError:
+            msgbox("File cannot saved.", 16)
+        
     def spotify_connect(self):
         if self.tokenExpired():
             self.spotify.refresh_token(self.spotify.get_credentials())
@@ -102,7 +141,9 @@ class Window(QtWidgets.QMainWindow):
         self.spotify.refresh_token(token)
     
     def getPlaylists(self):
+        self.button_export.setEnabled(False)
         playlists = self.spotify.get_playlists_songs()
+        self.playlists_dict = playlists
         return self.setTreeView(playlists)
     
     def setTreeView(self, playlists: dict):
@@ -117,8 +158,12 @@ class Window(QtWidgets.QMainWindow):
             
             Root.appendRow(playlist)
         
+        self.button_export.setEnabled(True)
         self.playlists_tree.setModel(model)
-
+        self.playlists_tree.clearSelection()
+    
+        model = self.playlists_tree.selectionModel()
+        model.selectionChanged.connect(self.changeSelected)
 
 app = QtWidgets.QApplication([])
 
